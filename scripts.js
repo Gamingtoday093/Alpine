f1c890bff578cc8175abed1075b3662f0bfb0081
Gamingtoday093 <Gamingtoday093@gmail.com>
2021-03-22 13:37:38 +0100

    Divisions

--- scripts.js
+++ scripts.js
@@ -6,14 +6,27 @@ var menu = document.getElementById("mobilemenu");
 
 function ShowMenu() {
 if (menu.style.display === "none") {
-menu.style.display = "block";
-button.style.transform = "rotatez(-180deg)";
-image.style.transform = "scaleY(0)";
-imageX.style.transform = "scaleY(1)";
+	menu.style.display = "block";
+	button.style.transform = "rotatez(-180deg)";
+	image.style.transform = "scaleY(0)";
+	imageX.style.transform = "scaleY(1)";
 } else {
-menu.style.display = "none";
-button.style.transform = "rotatez(0deg)";
-image.style.transform = "scaleY(1)";
-imageX.style.transform = "scaleY(0)";
+	menu.style.display = "none";
+	button.style.transform = "rotatez(0deg)";
+	image.style.transform = "scaleY(1)";
+	imageX.style.transform = "scaleY(0)";
+}
+}
+
+// Article Mobile Friendly Script
+var article1 = document.getElementById("Article1");
+var article2 = document.getElementById("Article2");
+function resizewin() {
+if($(window).width() <= 1048) {
+	article1.style.display = "block";
+	article2.style.display = "block";
+} else {
+	article1.style.display = "flex";
+	article2.style.display = "flex";
 }
 }
