c1717cf0ee369333741d4551d5f47b3c157830bd
Gamingtoday093 <Gamingtoday093@gmail.com>
2021-09-29 19:41:11 +0200

    Coupon Testing

--- store.js
+++ store.js
@@ -286,13 +286,14 @@ function showpaypal() {
 	allitemstostring();
 }
 
+var phptest = document.getElementById("phptest");
 function clearattention() {
 	steamid64.style = "";
-	steamid64.innerHTML = "";
+	phptest.innerHTML = "";
 	var xmlhttp = new XMLHttpRequest();
 	xmlhttp.onreadystatechange = function() {
 		if (this.readyState == 4 && this.status == 200) {
-			steamid64.innerHTML = this.responseText;
+			phptest.innerHTML = this.responseText;
 		}
 	};
 	var coupon = "test";
